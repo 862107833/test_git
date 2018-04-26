e232ff620da33156d16faf745d0389fc4d120e66
wb_gumingsheng <wb_gumingsheng@zhongan.io>
2018-04-26 10:34:08 +0800

    项目提交

---
+++ gms_test/gms_test/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  gms_test
+//
+//  Created by gumingsheng on 2018/2/28.
+//  Copyright © 2018年 GMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property(nonatomic, strong)UIView *view;
+
+
+
+
+@end
+
---
+++ gms_test/gms_test/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  gms_test
+//
+//  Created by gumingsheng on 2018/2/28.
+//  Copyright © 2018年 GMS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"我哦 水电费看速度快");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
